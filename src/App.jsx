7e03cb0fada1e62b77a85a7f4df6474ddd27b9f5
Jason M <jason.moposita1201@gmail.com>
2025-01-29 12:53:00 -0500

    Refactor to get any 5 unique elements from API

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,52 @@ const App = () => {
 	const [isGameOn, setIsGameOn] = useState(false)
    const [emojisData, setEmojisData] = useState([])
 
+   
 	async function startGame(e) {
-		e.preventDefault() // Prevents auto refresh
-
+      e.preventDefault() // Prevents auto refresh
+      
       try {
          const response = await fetch(`https://emojihub.yurace.pro/api/all/category/animals-and-nature`)
-
+         
          if(!response.ok) {
             throw new Error('Could not fetch data from API')
          }
-
-         const data = await response.json()
-         const dataSample = data.slice(0, 5)
          
-         setEmojisData(dataSample)
+         const data = await response.json()
+         const dataSlice = getDataSlice(data)
+         
+         setEmojisData(dataSlice)
          setIsGameOn(true)
       } catch (err) {
          console.error(err)
       }
 	}
+
+   function getDataSlice(data) {
+      const randomIndices = getRandomIndices(data)
+      const dataSlice = randomIndices.map(num => data[num])
+      return dataSlice
+   }
+   
+   function getRandomIndices(data) {
+      const randomIndicesArray = []
+
+      // Generate 5 UNIQUE numbers
+      for(let i = 0; i < 5; i++) {
+         const randomNum = Math.floor(Math.random() * data.length)
+
+         // If the number is NOT in the array, add it 
+         if (!randomIndicesArray.includes(randomNum)) {
+            randomIndicesArray.push(randomNum)
+         } else {
+            // If the number was already in the array
+            // Make sure there are still 5 elements in the array
+            i--
+         }
+      }
+
+      return randomIndicesArray
+   }
 
 	function turnCard() {
       console.log('Card clicked')
@@ -34,7 +61,7 @@ const App = () => {
 		<main>
 			<h1>Memory</h1>
 			{!isGameOn && <Form handleSubmit={startGame} />}
-			{isGameOn && <MemoryCard handleClick={turnCard} />}
+			{isGameOn && <MemoryCard data={emojisData} handleClick={turnCard} />}
 		</main>
 	)
 }
