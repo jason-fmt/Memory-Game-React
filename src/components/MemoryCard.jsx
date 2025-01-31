daafb33728e7adafb7c2638d03f590682f75e6d2
Jason M <jason.moposita1201@gmail.com>
2025-01-30 20:52:02 -0500

    Set className based on condition & pass down state

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -6,13 +6,20 @@ import EmojiButton from "./EmojiButton"
 const MemoryCard = ({ data, handleClick, selectedCards, matchedCards }) => {
    // Stores list of buttons containing an emoji from the emojiArray
 	const cardElements = data.map((emojiObj, index) => {
+      const selectedCardEntry = selectedCards.find(emoji => emoji.index === index)
+      const matchedCardEntry = matchedCards.find(emoji => emoji.index === index)
 
+      let cardStyle = ''
+      if(selectedCardEntry) cardStyle = 'card-item--selected'
+      if(matchedCardEntry) cardStyle = 'card-item--matched'
+      
       return (
-		   <li key={index} className='card-item'>
+		   <li key={index} className={`card-item ${cardStyle}`}>
 			   <EmojiButton 
                content={decodeEntity(emojiObj.htmlCode[0])}
-               style={'btn btn--emoji'}
                handleClick={() => handleClick(emojiObj.name, index)}
+               isSelected={selectedCardEntry}
+               isMatched={matchedCardEntry}
             />
 		   </li>
 	   )
