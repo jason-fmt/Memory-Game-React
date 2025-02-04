33786401a35f8c1475b8327c7cd7200ac59904fa
Jason M <jason.moposita1201@gmail.com>
2025-02-03 20:28:01 -0500

    Make form elements into reusable components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,24 @@
+import { useEffect, useRef } from 'react'
 import RegularButton from './RegularButton'
+import Select from './Select'
 
-const Form = ({ handleSubmit }) => {
+const Form = ({ handleSubmit, handleChange, isFirstRender }) => {
+	const divRef = useRef(null)
+
+	useEffect(() => {
+		!isFirstRender && divRef.current.focus()
+	}, [])
+
 	return (
-		<form className='wrapper'>
-			<RegularButton handleClick={handleSubmit}>
-            Start Game
-         </RegularButton>
-		</form>
+		<div className='form-container' ref={divRef} tabIndex={-1}>
+			<p className="p--regular">
+				Customize the game by selecting an emoji category and a number of memory cards.
+			</p>
+			<form className='wrapper'>
+				<Select handleChange={handleChange} />
+				<RegularButton handleClick={handleSubmit}>Start Game</RegularButton>
+			</form>
+		</div>
 	)
 }
 
